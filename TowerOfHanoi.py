be72962eda8eb242b7d516af77287bddb8927bc9
him9666 <kaminipatyhimesh9666@gmail.com>
2021-10-01 09:10:57 +0530

    Create TowerOfHanoi.py
    TowerOfHanoi algorithm implementation recursively

---
+++ TowerOfHanoi.py
@@ -0,0 +1,13 @@
+# this program solves tower of hanoi in  Recursively fashion with 3 rods 
+
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
+	if n == 1: 
+		print "Move disk 1 from rod",from_rod,"to rod",to_rod 
+		return
+	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
+	print "Move disk",n,"from rod",from_rod,"to rod",to_rod 
+	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
+
+
+n = 4  #input n is the no.of disks 
+TowerOfHanoi(n, 'A', 'C', 'B')    # A,B,C are the rods used
